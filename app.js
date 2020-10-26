c6bfadb85deb0ac9822cb61e91bd995316d04a96
unknown <katiajaafar28@gmail.com>
2020-10-26 19:30:16 +0200

    ...

--- app.js
+++ app.js
@@ -35,16 +35,20 @@ app.get('/api/en', (req, res) => {
     console.log(err)
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'ang-app', 'en', 'index.html'));
+})
+
 //app.get('*.*', express.static(distDir));
-if(process.env.NODE_ENV === 'production') {
-    const distDir = __dirname + "/dist/";
-    app.use(express.static(distDir));
-    app.use('/api/de',  express.static(path.resolve(__dirname, 'dist', 'ang-app', 'de')));
-    app.use('/api/en',  express.static(path.resolve(__dirname, 'dist', 'ang-app', 'en')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'dist', 'ang-app', 'en', 'index.html'));
-    })
-}
+// if(process.env.NODE_ENV === 'production') {
+//     const distDir = __dirname + "/dist/";
+//     app.use(express.static(distDir));
+//     app.use('/de',  express.static(path.resolve(__dirname, 'dist', 'ang-app', 'de')));
+//     app.use('/en',  express.static(path.resolve(__dirname, 'dist', 'ang-app', 'en')));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'dist', 'ang-app', 'en', 'index.html'));
+//     })
+// }
 
 app.listen(port, () => {
     console.log('Server listening...');
